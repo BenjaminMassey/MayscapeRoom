962162cf12b2031b942a86c7bad00f2e4d3395e1
Benjamin Massey <benjamin.w.massey@gmail.com>
2023-07-07 19:16:21 -0700

    Took cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,7 @@ struct Pos {
 
 impl Pos {
     fn new(x: f32, y: f32) -> Self {
-        Pos {
-            x,
-            y,
-        }
+        Pos { x, y }
     }
 }
 
@@ -24,7 +21,13 @@ struct Item {
 }
 
 impl Item {
-    fn new(tag: &str, texture: Texture2D, position: Pos, flavor_text: &str, link: Option<Box<Item>>) -> Self {
+    fn new(
+        tag: &str,
+        texture: Texture2D,
+        position: Pos,
+        flavor_text: &str,
+        link: Option<Box<Item>>,
+    ) -> Self {
         Item {
             tag: tag.to_owned(),
             texture,
@@ -37,34 +40,25 @@ impl Item {
 
 #[macroquad::main("EscapeRoom")]
 async fn main() {
-
     let mut items: Vec<Item> = Vec::new();
-    
-    let door_texture: Texture2D =
-        load_texture("assets/ExitDoor.png")
-        .await
-        .unwrap();
+
+    let door_texture: Texture2D = load_texture("assets/ExitDoor.png").await.unwrap();
 
     let door = Item::new(
         "exit_door",
         door_texture,
         Pos::new(100f32, 20f32),
         "I don't really feel like leaving the room, actually",
-        None
+        None,
     );
 
     items.push(door);
-    
+
     loop {
         clear_background(DARKGRAY);
 
         for item in &items {
-            draw_texture(
-                item.texture,
-                item.position.x,
-                item.position.y,
-                WHITE,
-            );
+            draw_texture(item.texture, item.position.x, item.position.y, WHITE);
         }
 
         next_frame().await
