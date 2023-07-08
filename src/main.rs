94eb845062bcfbfb9d4971d3c42997bc555f5b41
Benjamin Massey <benjamin.w.massey@gmail.com>
2023-07-07 19:15:49 -0700

    Have start of more real code, where Item is a thing that exists, and the drawing draws each item each frame: plus first image being truly drawn yay

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,71 @@
 use macroquad::prelude::*;
+use macroquad::texture::Texture2D;
 
-#[macroquad::main("BasicShapes")]
+struct Pos {
+    x: f32,
+    y: f32,
+}
+
+impl Pos {
+    fn new(x: f32, y: f32) -> Self {
+        Pos {
+            x,
+            y,
+        }
+    }
+}
+
+struct Item {
+    tag: String,
+    texture: Texture2D,
+    position: Pos,
+    flavor_text: String,
+    link: Option<Box<Item>>,
+}
+
+impl Item {
+    fn new(tag: &str, texture: Texture2D, position: Pos, flavor_text: &str, link: Option<Box<Item>>) -> Self {
+        Item {
+            tag: tag.to_owned(),
+            texture,
+            position,
+            flavor_text: flavor_text.to_owned(),
+            link,
+        }
+    }
+}
+
+#[macroquad::main("EscapeRoom")]
 async fn main() {
+
+    let mut items: Vec<Item> = Vec::new();
+    
+    let door_texture: Texture2D =
+        load_texture("assets/ExitDoor.png")
+        .await
+        .unwrap();
+
+    let door = Item::new(
+        "exit_door",
+        door_texture,
+        Pos::new(100f32, 20f32),
+        "I don't really feel like leaving the room, actually",
+        None
+    );
+
+    items.push(door);
+    
     loop {
-        clear_background(RED);
+        clear_background(DARKGRAY);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        for item in &items {
+            draw_texture(
+                item.texture,
+                item.position.x,
+                item.position.y,
+                WHITE,
+            );
+        }
 
         next_frame().await
     }
